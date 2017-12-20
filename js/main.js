3b5fcbc44c6d7988e12cee059ebfc2289ec7617d
zzaptv <thezzaptv@gmail.com>
2017-12-20 09:32:58 +0100

    rename

--- js/main.js
+++ js/main.js
@@ -124,5 +124,12 @@ function showDuration() {
         }
         $('#progress').css('width', progress + '%');
     });
+    }
 
+    function calculateTotalValue(length) {
+        var minutes = Math.floor(length / 60),
+        seconds_int = length - minutes * 60,
+        seconds_str = seconds_int.toString(),
+        seconds = seconds_str.substr(0, 2),
+        time = minutes + ':' + seconds;
 }
